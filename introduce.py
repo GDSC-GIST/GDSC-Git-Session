316ce84ecaec30263e25b2e730270e9eb2796261
김산 <kimsan@gimsan-ui-MacBookAir.local>
2023-01-14 22:42:07 +0900

    first commit

--- introduce.py
+++ introduce.py
@@ -1,9 +1,9 @@
 # 이 함수에서 자기소개를 해주세요.
 def introduce():
     # TODO : name에는 본인의 이름을, intro에는 본인의 자기소개를 넣어주세요.
-    name = 'Juchan Lee'
-    intro = '저는 미쿡사람입니다. 방가워요!'
-    print(f"안녕하세요 저는 {name}입니다. {intro}")
+    name = 'San'
+    intro = '저는 창원사람입니다. 방가워요!'
+    print(f"안녕하세요 {name}입니다. {intro}")
 
 # 이 아래 부분은 수정하지 마세요.
 if __name__ == "__main__":
