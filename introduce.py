56ba2d16f0747b57273e6d030454a80937cbc7e2
Do-hyun Ko <dohyun682@gmail.com>
2023-01-14 22:42:36 +0900

    도현 자기소개

--- introduce.py
+++ introduce.py
@@ -1,8 +1,8 @@
 # 이 함수에서 자기소개를 해주세요.
 def introduce():
     # TODO : name에는 본인의 이름을, intro에는 본인의 자기소개를 넣어주세요.
-    name = 'Juchan Lee'
-    intro = '저는 미쿡사람입니다. 방가워요!'
+    name = 'Dohyun Ko'
+    intro = '저는 한국사람입니다. 방가워요!'
     print(f"안녕하세요 저는 {name}입니다. {intro}")
 
 # 이 아래 부분은 수정하지 마세요.
